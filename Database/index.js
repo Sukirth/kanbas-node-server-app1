364c1b417910009408ebb5539ba095a5e2bedbf6
akuatbekov23 <akuatbekov23@gmail.com>
2023-12-04 22:15:50 -0500

    a5

---
+++ Database/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+import courses from "./courses.json";
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import users from "./users.json";
+import grades from "./grades.json";
+import enrollments from "./enrollment.json";
+export default {
+  courses,
+  modules,
+  assignments,
+  users,
+  grades,
+  enrollments,
+};
